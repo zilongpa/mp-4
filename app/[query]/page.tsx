7d162954317bc21fe4debccfbe4fc90f39bc9ab1
zilongpa <hjh604@outlook.com>
2025-03-29 19:58:14 -0400

    Purge unused resources

---
+++ app/[query]/page.tsx
@@ -0,0 +1,4 @@
+export default function Result() {
+    return <>result</>;
+}
+  
---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function Header() {
+    return (
+    <header>
+        <h3>CS391 Icon Finder</h3>
+        <nav>
+            <Link href="/">Home</Link>
+        </nav>
+    </header>
+    )
+}
